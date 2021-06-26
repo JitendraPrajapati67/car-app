6fd97d1bd292cf76c0db1e735672bb5cb980fb90
jitendrap67 <jitendra@rivuletdigital.com>
2021-06-26 08:38:16 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-dark">
+
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
